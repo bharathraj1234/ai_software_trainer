b93e760a0bf95cf11d004ec752c61511ed642c3d
bharathraj1234 <tbharathraj1234@gmail.com>
2026-06-26 22:59:07 +0530

    piper speaking added

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
 import requests
 import json
+from pathlib import Path
+import wave
+from piper import PiperVoice
+import subprocess
+
+# Folder containing this script
+BASE = Path(__file__).resolve().parent
+
+# Tamil model
+MODEL = BASE.parent / "models" / "piper" / "ta_IN-Valluvar-medium.onnx"
+
+print("Loading model...")
+voice = PiperVoice.load(str(MODEL))
+print("Model loaded!")
+
 
 # Store the entire conversation
 messages = [
@@ -13,6 +28,16 @@ print("=" * 50)
 print("        Gemma AI Chatbot")
 print("Type 'exit' to quit.")
 print("=" * 50)
+
+
+def speak(text):
+
+    # Create output.wav
+    with wave.open("output.wav", "wb") as wav_file:
+        voice.synthesize_wav(text, wav_file)
+        print("Done!")
+        print("Saved as output.wav")
+        subprocess.run(["aplay", "output.wav"])
 
 while True:
 
@@ -34,7 +59,8 @@ while True:
                         Guide users.
                         One step at a time.
                         try to respond in short instructions or ask more question no too much epxlanation needed
-                        dont add * in the response""" + user_input
+                        dont add * in the response
+                        strictly only use tamil language in response""" + user_input
         }
     )
 
@@ -66,6 +92,12 @@ while True:
                 print(chunk, end="", flush=True)
 
                 assistant_reply += chunk
+                 # Generation finished
+                if data.get("done"):
+
+                    print("\n\nGeneration Complete!")
+
+                    speak(assistant_reply)
 
    
 
@@ -76,3 +108,7 @@ while True:
             "content": assistant_reply
         }
     )
+
+
+
+
